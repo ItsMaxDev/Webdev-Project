ff5cf6de2bad8fe0ae6076b8c24c46afa24df327
Max Kruiswegt <112982786+ItsMaxDev@users.noreply.github.com>
2023-12-07 19:03:15 +0100

    Updated SQL Script

---
+++ sql/webdev_project.sql
@@ -0,0 +1,27 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+-- Create database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS `webdev_project`;
+
+-- Switch to the database
+USE `webdev_project`;
+
+-- Table structure for table `article`
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Indexes for table `users`
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+-- AUTO_INCREMENT for table `users`
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
