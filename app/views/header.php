72d73eb332aeaadbe4c496f04fb82674187d284f
Max Kruiswegt <112982786+ItsMaxDev@users.noreply.github.com>
2023-12-10 16:44:29 +0100

    Update Title

--- app/views/header.php
+++ app/views/header.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="dark">
 <head>  
-  <title>MVC Blog</title>
+  <title>Webdev-Project</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
