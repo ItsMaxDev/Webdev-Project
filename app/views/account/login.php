1894d6dce381376b5940d8fd65ce7d92776bd1a2
Max Kruiswegt <112982786+ItsMaxDev@users.noreply.github.com>
2023-12-15 17:11:47 +0100

    Refactor login and signup forms

--- app/views/account/login.php
+++ app/views/account/login.php
@@ -10,24 +10,28 @@ include_once __DIR__ . '/../../helpers/session_helper.php';
 
 <div class="container mt-5">
     <div class="row justify-content-center">
-        <div class="col-sm-8 col-md-6 col-lg-5 col-xl-4 bg-dark-subtle p-4 rounded shadow">
-            <?php flash('login'); ?>
-            <h1 class="text-center">Login</h1>
-            <p class="text-center text-secondary-emphasis">Don't have an account? <a class="text-secondary-emphasis" href="/account/signup">Sign up</a></p>
-            <form method="post">
-                <div class="form-group mt-4 mb-3">
-                    <label for="username">Username/Email</label>
-                    <input name="username/email" id="username/email" class="form-control" />
+        <div class="col-sm-8 col-md-6 col-lg-5 col-xl-4">
+            <div class="card">
+                <div class="card-body">
+                    <?php flash('login'); ?>
+                    <h1 class="text-center">Login</h1>
+                    <p class="text-center">Don't have an account? <a href="/account/signup">Sign up</a></p>
+                    <form method="post">
+                        <div class="form-group mt-4 mb-3">
+                            <label for="username">Username/Email</label>
+                            <input name="username/email" id="username/email" class="form-control" />
+                        </div>
+                        <div class="form-group mb-4">
+                            <div class="d-flex justify-content-between">
+                                <label for="password">Password</label>
+                                <a href="/account/forgotpassword">Forgot Password?</a>
+                            </div>
+                            <input name="password" id="password" class="form-control" type="password" />
+                        </div>
+                        <button type="submit" class="btn btn-primary rounded-pill col-12" value="login">Login</button>
+                    </form>
                 </div>
-                <div class="form-group mb-4">
-                    <div class="d-flex justify-content-between">
-                        <label for="password">Password</label>
-                        <a class="text-secondary-emphasis text-decoration-none" href="/account/forgotpassword">Forgot Password?</a>
-                    </div>
-                    <input name="password" id="password" class="form-control" type="password" />
-                </div>
-                <button type="submit" class="btn btn-primary rounded-pill col-12" value="login">Login</button>
-            </form>
+            </div>
         </div>
     </div>
 </div>
--- app/views/account/signup.php
+++ app/views/account/signup.php
@@ -10,33 +10,37 @@ include_once __DIR__ . '/../../helpers/session_helper.php';
 
 <div class="container mt-5">
     <div class="row justify-content-center">
-        <div class="col-sm-8 col-md-6 col-lg-5 col-xl-4 bg-dark-subtle p-4 rounded shadow">
-            <?php flash('register'); ?>
-            <h1 class="text-center">Sign Up</h1>
-            <p class="text-center text-secondary-emphasis">Already have an account? <a class="text-secondary-emphasis" href="/account/login">Login</a></p>
-            <form method="post">
-                <div class="form-group mt-4 mb-3">
-                    <label for="name">Name</label>
-                    <input name="name" id="name" class="form-control" />
+        <div class="col-sm-8 col-md-6 col-lg-5 col-xl-4">
+            <div class="card">
+                <div class="card-body">
+                    <?php flash('register'); ?>
+                    <h1 class="text-center">Sign Up</h1>
+                    <p class="text-center">Already have an account? <a href="/account/login">Login</a></p>
+                    <form method="post">
+                        <div class="form-group mt-4 mb-3">
+                            <label for="name">Name</label>
+                            <input name="name" id="name" class="form-control" />
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="email">Email</label>
+                            <input name="email" id="email" class="form-control" />
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="username">Username</label>
+                            <input name="username" id="username" class="form-control" />
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="password">Password</label>
+                            <input name="password" id="password" class="form-control" type="password" />
+                        </div>
+                        <div class="form-group mb-4">
+                            <label for="confirmPassword">Confirm Password</label>
+                            <input name="confirmPassword" id="confirmPassword" class="form-control" type="password" />
+                        </div>
+                        <button type="submit" class="btn btn-primary rounded-pill col-12" value="signup">Sign Up</button>
+                    </form>
                 </div>
-                <div class="form-group mb-3">
-                    <label for="email">Email</label>
-                    <input name="email" id="email" class="form-control" />
-                </div>
-                <div class="form-group mb-3">
-                    <label for="username">Username</label>
-                    <input name="username" id="username" class="form-control" />
-                </div>
-                <div class="form-group mb-3">
-                    <label for="password">Password</label>
-                    <input name="password" id="password" class="form-control" type="password" />
-                </div>
-                <div class="form-group mb-4">
-                    <label for="confirmPassword">Confirm Password</label>
-                    <input name="confirmPassword" id="confirmPassword" class="form-control" type="password" />
-                </div>
-                <button type="submit" class="btn btn-primary rounded-pill col-12" value="signup">Sign Up</button>
-            </form>
+            </div>
         </div>
     </div>
 </div>
