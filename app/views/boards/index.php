1db0706c7587260168e06b561f60573216508fe7
Max Kruiswegt <112982786+ItsMaxDev@users.noreply.github.com>
2023-12-15 17:11:34 +0100

    Add board creation card to boards page

--- app/views/boards/index.php
+++ app/views/boards/index.php
@@ -9,7 +9,16 @@ include_once __DIR__ . '/../../helpers/session_helper.php';
 <?php endif; ?>
 
 <?php flash('boards'); ?>
-<h1>Boards</h1>
+<h1 class="mt-5"><?php echo $_SESSION['user_username'] . "'s boards"; ?></h1>
+
+<!-- Card for adding a board -->
+<div class="col-lg-3 col-md-6 col-sm-12">
+    <a href="#" class="card text-decoration-none">
+        <div class="card-body">
+            <h5 class="card-title">Add a Board</h5>
+        </div>
+    </a>
+</div>
 
 <?php
 include_once __DIR__ . '/../footer.php';
