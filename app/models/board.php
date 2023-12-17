7dd3daae5fbeee4d05ecd82164692c8592dda42d
Max Kruiswegt <112982786+ItsMaxDev@users.noreply.github.com>
2023-12-17 15:50:35 +0100

    Add new models: Board, Card, and List

---
+++ app/models/board.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Models;
+
+class Board {
+    public $id;
+    public $userId;
+    public $name;
+    public $description;
+}
---
+++ app/models/card.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+
+class Card {
+    public $id;
+    public $listId;
+    public $name;
+    public $description;
+    public $dueDate;
+}
