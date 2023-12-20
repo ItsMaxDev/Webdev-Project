91538f2602d04f56a982eda487393089fa1a17f9
Max Kruiswegt <112982786+ItsMaxDev@users.noreply.github.com>
2023-12-20 17:27:53 +0100

    Add API routes

--- app/patternrouter.php
+++ app/patternrouter.php
@@ -13,6 +13,12 @@ class PatternRouter
 
     public function route($uri)
     {
+        $api = false;
+        if (str_starts_with($uri, "api/")) {
+            $uri = substr($uri, 4);
+            $api = true;
+        }
+
         $uri = $this->stripParameters($uri);
 
         $explodedUri = explode('/', $uri);
@@ -20,7 +26,7 @@ class PatternRouter
         if (!isset($explodedUri[0]) || empty($explodedUri[0])) {
             $explodedUri[0] = 'home';
         }
-        $controllerName = "App\\Controllers\\" . $explodedUri[0] . "controller";
+        $controllerName = "App\\" . ($api ? "Api\\" : "") . "Controllers\\" . $explodedUri[0] . "controller";
 
         if (!isset($explodedUri[1]) || empty($explodedUri[1])) {
             $explodedUri[1] = 'index';
