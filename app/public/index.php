64d27fdea9605fb00e87c0e6303e141096fa768a
Max Kruiswegt <112982786+ItsMaxDev@users.noreply.github.com>
2024-01-21 14:23:53 +0100

    Improve exception handling using buffer

--- app/public/index.php
+++ app/public/index.php
@@ -1,13 +1,20 @@
 <?php
 
 ini_set('display_errors', 'Off');
+
+// Start output buffering
+ob_start();
+
 try {
     require '../vendor/autoload.php';
     session_start();
     $uri = trim($_SERVER['REQUEST_URI'], '/');
     $router = new App\PatternRouter();
     $router->route($uri);
+
+    // Flush the output buffer and send to the browser
+    ob_end_flush();
 } catch (\Throwable $exception) {
-    $errorMessage = $exception->getMessage();
-    require __DIR__ . '/../views/exception.php';
+    ob_clean();
+    require_once __DIR__ . '/../views/exception.php';
 }
--- app/views/exception.php
+++ app/views/exception.php
@@ -1,15 +1,17 @@
 <?php
-include_once __DIR__ . '/header.php';
+include __DIR__ . '/header.php';
 ?>
 
-<div class="row justify-content-center">
-    <div class="col-sm-10 col-md-8 col-lg-7 col-xl-6">
-        <div class="card">
-            <div class="card-body">
-                <h1 class="text-center">Something went wrong!</h1>
-                <div class="alert alert-danger mt-4"><?php echo $errorMessage;?></div>
-                <div class="text-center mt-3">
-                    <a href="/" class="btn btn-primary">Go back to homepage</a>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-sm-10 col-md-8 col-lg-7 col-xl-6">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="text-center">Something went wrong!</h1>
+                    <div class="alert alert-danger mt-4"><?php echo $exception;?></div>
+                    <div class="text-center mt-3">
+                        <a href="/" class="btn btn-primary">Go back to homepage</a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -17,5 +19,5 @@ include_once __DIR__ . '/header.php';
 </div>
 
 <?php
-include_once __DIR__ . '/footer.php';
+include __DIR__ . '/footer.php';
 ?>
